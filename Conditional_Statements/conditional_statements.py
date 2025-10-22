fd9490b6d648b6a53021a3e042fbcf16048d653a
Zakaria-Belkouche <zackbelk16@gmail.com>
2025-10-22 22:29:51 +0100

    ConditionalStatements

---
+++ Conditional_Statements/conditional_statements.py
@@ -0,0 +1,24 @@
+# Zakaria Belkouche 22/10/25
+
+x = input("What is your last name?: ")
+
+
+if x[0] in ("A", "B", "C", "D", "a", "b", "c", "d"):
+    print("Aha! You're on the team Red Dragon")
+
+if x[0] in ("E", "F", "G", "H", "e", "f", "g", "h"):
+    print("Aha! You're on the team Dark Wizards")
+
+if x[0] in ("I", "J", "K", "L", "i", "j", "k", "l"):
+    print("Aha! You're on the team Moving Castle")
+
+if x[0] in ("M", "N", "O", "P", "m", "n", "o", "p"):
+    print("Aha! You're on the team Golden Snitches")
+
+if x[0] in ("Q", "R", "S", "T", "q", "r", "s", "t"):
+    print("Aha! You're on the team Night Guards")
+
+if x[0] in ("U", "V", "W", "X", "Y", "Z", "u", "v", "w", "x", "y", "z"):
+    print("Aha! You're on the team Black Holes")
+
+print("Good luck in the games!")
